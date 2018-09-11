c1f029369066e413de4e770fadb9a2ca4c237968
Simge Ergüden <s-erguden@tkb.local>
2018-09-11 14:54:13 +0300

    first commit

---
+++ src/app/Models/Todo.ts
@@ -0,0 +1,5 @@
+export class Todo{
+
+    ItemName : string;
+    Completed: boolean;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from './Models/Todo' ; 
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TodoApp';
+  ItemList :Array<Todo> = [];
+  Item: Todo = {Completed: false, ItemName :''};
+  AddItem(){
+   console.log("Eklenen item:"+ this.Item.ItemName);
+   this.ItemList.push({'Completed' : this.Item.Completed,'ItemName': this.Item.ItemName});
+   console.log(this.ItemList);
+   this.Item.ItemName = ''; 
+  }
+  ItemCompleted(todo Todo){
+    this.ItemList.find(x=>x.ItemName == todo.ItemName).Completed = true;
+    console.log(this.ItemList);
+  }
 }
+
